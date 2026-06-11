0e41e9015fd2f0d3e861a09eb5551f7f04014a6a
punith-webknot <punith.k@webknot.in>
2026-06-11 17:11:21 +0530

    *******REVAMP********

--- Source/app/llms/openai_client.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import ChatOpenAI
-
-from app.api.schemas import SupervisorResponse
-
-def get_supervisor_llm():
-    return ChatOpenAI(
-        model="gpt-4o-mini",
-        temperature=0,
-    ).with_structured_output(SupervisorResponse)
--- Source/app/state/graph_state.py
+++
@@ -1,8 +0,0 @@
-from typing_extensions import TypedDict
-
-from app.utils.constants import AgentName
-
-
-class GraphState(TypedDict):
-    messages: list[str]
-    next_agent: AgentName
